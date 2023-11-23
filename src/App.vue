5ac6957eb5ae8827ce922e0b0bade3517661ed83
Od saleva <misha.vidiakin@gmail.com>
2023-11-23 15:48:25 +0300

    save

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,101 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+import { onMounted, ref } from "vue";
+
+const API_KEY = "fca_live_INn8oonmjCvqkWaIfZgUSc7dBEfazBHAxrz8EKUC";
+const API_URL = "https://api.freecurrencyapi.com/v1/";
+const BASE_CURRENCY = "RUB";
+const userStatus = ref("");
+const currencies = ref("");
+const currenciesСoefficient = ref("");
+const err = ref(false);
+let exchangeСlculationResult = 0;
+
+onMounted(async () => {
+  await fetch(`${API_URL}status?apikey=${API_KEY}`)
+    .then((response) => response.json())
+    .then((json) => {
+      userStatus.value = json.quotas.month;
+    })
+    .catch((e) => {
+      err.value = true;
+    });
+  await fetch(`${API_URL}currencies?&apikey=${API_KEY}`)
+    .then((response) => response.json())
+    .then((json) => {
+      currencies.value = json.data;
+    })
+    .catch((e) => {
+      err.value = true;
+    });
+  await fetch(
+    `${API_URL}latest?base_currency=${BASE_CURRENCY}&apikey=${API_KEY}`
+  )
+    .then((response) => response.json())
+    .then((json) => {
+      currenciesСoefficient.value = json.data;
+    })
+    .catch((e) => {
+      err.value = true;
+    });
+});
+
+const exchangeСlculation = (key) => {
+  let i = 1;
+  exchangeСlculationResult = (i / currenciesСoefficient.value[key]).toFixed(2);
+  for (i = 1; exchangeСlculationResult < 1; i *= 10) {
+    exchangeСlculationResult = (i / currenciesСoefficient.value[key]).toFixed(
+      2
+    );
+  }
+  return i;
+};
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <header class="header">
+    <div class="header__logo">
+      <RouterLink :to="{ name: 'Home' }">
+        <img src="" alt="Logo" />
+      </RouterLink>
+    </div>
+    <nav class="header__nav">
+      <RouterLink :to="{ name: 'Home' }">Home</RouterLink>
+    </nav>
+    <ul v-if="!err" class="header__user-status user-status">
+      <li
+        v-for="[key, value] in Object.entries(userStatus)"
+        :key="key"
+        class="user-status__row"
+      >
+        <div class="user-status__key">{{ key }}:</div>
+        <div class="user-status__value">
+          {{ value }}
+        </div>
+      </li>
+    </ul>
+    <div v-else class="err">not found</div>
+  </header>
+  <hr />
+  <main class="main">
+    <form action="">
+      <input type="text" />
+    </form>
+    <section class="main__currencies currencies">
+      <div
+        v-for="[key, value] in Object.entries(currencies)"
+        :key="key"
+        class="currencies__item"
+      >
+        <div class="currencies__name">
+          {{ value.name }}
+        </div>
+        <div class="currencies__currencies-сoefficient">
+          {{ exchangeСlculation(key) }} - {{ key }}
+        </div>
+        <div class="currencies__currencies-сoefficient">
+          {{ exchangeСlculationResult }}
+        </div>
+      </div>
+    </section>
+  </main>
 </template>
-
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
