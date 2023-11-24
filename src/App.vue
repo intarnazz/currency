926672d9b2531b3c44a52c38bf629306e2743505
Od saleva <misha.vidiakin@gmail.com>
2023-11-24 12:58:40 +0300

    save

--- src/App.vue
+++ src/App.vue
@@ -3,15 +3,31 @@ import { onMounted, ref } from "vue";
 
 const API_KEY = "fca_live_INn8oonmjCvqkWaIfZgUSc7dBEfazBHAxrz8EKUC";
 const API_URL = "https://api.freecurrencyapi.com/v1/";
-const BASE_CURRENCY = "RUB";
 const DATE__HISTORICAL = "2022-01-01";
 const currenciesHistorocal = ref("");
 const userStatus = ref("");
 const currencies = ref("");
 const currenciesСoefficient = ref("");
+const filter = ref("");
 const err = ref(false);
+const baseCurrencyClose = ref(false);
+const baseCurrency = ref("RUB");
+const deltaHisObg = ref({});
 let exchangeСlculationResult = 0;
 let delHis = 0;
+
+function historicalCurrencies() {
+  fetch(
+    `${API_URL}historical?date=${DATE__HISTORICAL}&base_currency=${baseCurrency.value}&apikey=${API_KEY}`
+  )
+    .then((response) => response.json())
+    .then((json) => {
+      currenciesHistorocal.value = json.data[DATE__HISTORICAL];
+    })
+    .catch((e) => {
+      err.value = true;
+    });
+}
 
 onMounted(async () => {
   await fetch(`${API_URL}status?apikey=${API_KEY}`)
@@ -22,16 +38,7 @@ onMounted(async () => {
     .catch((e) => {
       err.value = true;
     });
-  await fetch(
-    `${API_URL}historical?date=${DATE__HISTORICAL}&base_currency=${BASE_CURRENCY}&apikey=${API_KEY}`
-  )
-    .then((response) => response.json())
-    .then((json) => {
-      currenciesHistorocal.value = json.data[DATE__HISTORICAL];
-    })
-    .catch((e) => {
-      err.value = true;
-    });
+  historicalCurrencies();
   await fetch(`${API_URL}currencies?&apikey=${API_KEY}`)
     .then((response) => response.json())
     .then((json) => {
@@ -41,7 +48,7 @@ onMounted(async () => {
       err.value = true;
     });
   await fetch(
-    `${API_URL}latest?base_currency=${BASE_CURRENCY}&apikey=${API_KEY}`
+    `${API_URL}latest?base_currency=${baseCurrency.value}&apikey=${API_KEY}`
   )
     .then((response) => response.json())
     .then((json) => {
@@ -59,13 +66,26 @@ const exchangeСlculation = (key) => {
   while (i / coefficient < 10) {
     i *= 10;
   }
-  exchangeСlculationResult = (i / coefficient).toFixed(2);
+  deltaHisObg.value[key].price = (i / coefficient).toFixed(2);
   return i;
 };
-const deltaCurrencies = (actual, historocal) => {
-  console.log(actual, " - ", historocal);
-  delHis = (actual - historocal).toFixed(4);
-  return delHis;
+const deltaCurrencies = (actual, historocal, key) => {
+  deltaHisObg.value[key] = {
+    value: (actual - historocal).toFixed(4),
+    price: null,
+  };
+  return deltaHisObg.value[key].value;
+};
+const baseCurrencyEvent = () => {
+  if (baseCurrencyClose.value) {
+    baseCurrencyClose.value = false;
+  } else {
+    baseCurrencyClose.value = true;
+  }
+};
+const baseCurrencyChange = (key) => {
+  baseCurrency.value = key;
+  historicalCurrencies();
 };
 </script>
 
@@ -95,34 +115,71 @@ const deltaCurrencies = (actual, historocal) => {
   </header>
   <hr />
   <main class="main">
-    <form action="">
-      <input type="text" />
-    </form>
-    <section class="main__currencies currencies">
-      <div
-        v-for="[key, value] in Object.entries(currencies)"
-        :key="key"
-        class="currencies__item"
+    <div class="main__all-currencies all-currencies">
+      <i>Список свех валют </i>
+      <button
+        @click="baseCurrencyEvent()"
+        class="button all-currencies__button_main"
       >
-        <div class="currencies__name">
-          {{ value.name }}
-        </div>
+        {{ baseCurrency }}
+      </button>
+
+      <div
+        :class="{ popup__close: baseCurrencyClose }"
+        class="all-currencies__popup popup"
+      >
         <div
-          class="currencies__currencies-сoefficient rise"
-          :class="{
-            fall:
-              deltaCurrencies(
-                currenciesСoefficient[key],
-                currenciesHistorocal[key]
-              ) < 0,
-          }"
+          v-for="[key] in Object.entries(currencies)"
+          :key="key"
+          class="all-currencies-item"
         >
-          {{ exchangeСlculation(key) }} - {{ key }} ({{ delHis }})
-        </div>
-        <div class="currencies__currencies-сoefficient">
-          {{ exchangeСlculationResult }} - RUB
+          <button
+            @click="baseCurrencyChange(key)"
+            class="all-currencies__button button"
+          >
+            {{ key }}
+          </button>
         </div>
       </div>
+    </div>
+    <form action="">
+      <input v-model="filter" type="text" />
+    </form>
+    <section class="main__currencies currencies">
+      <template v-for="[key, value] in Object.entries(currencies)" :key="key">
+        <div
+          v-if="value.name.toLowerCase().includes(filter.toLowerCase())"
+          class="currencies__item"
+        >
+          <div class="currencies__name">
+            {{ value.name }}
+          </div>
+          <div
+            class="currencies__currencies-сoefficient rise"
+            :class="{
+              fall:
+                deltaCurrencies(
+                  currenciesСoefficient[key],
+                  currenciesHistorocal[key],
+                  key
+                ) < 0,
+            }"
+          >
+            {{ exchangeСlculation(key) }} - {{ key }} ({{
+              deltaHisObg[key].value
+            }})
+            <span v-if="delHis >= 0" class="material-symbols-outlined">
+              arrow_upward
+            </span>
+            <span v-else class="material-symbols-outlined">
+              arrow_downward
+            </span>
+          </div>
+          <div class="currencies__currencies-сoefficient">
+            {{ deltaHisObg[key].price }} - {{ baseCurrency }}
+          </div>
+        </div>
+      </template>
     </section>
   </main>
 </template>
